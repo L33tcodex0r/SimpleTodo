531315f78cd0b2364c164f7ae68444ec6caacf32
Alexander Tu <alextu87@gmail.com>
2015-01-24 12:54:01 -0800

    Initial commit

---
+++ app/src/main/java/com/atuproject/simpletodo/EditItemActivity.java
@@ -0,0 +1,60 @@
+package com.atuproject.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class EditItemActivity extends ActionBarActivity {
+    EditText etEditItem;
+    int position;
+    String item;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        etEditItem = (EditText) findViewById(R.id.etEditItem);
+        String item = getIntent().getStringExtra("item");
+        etEditItem.setText(item);
+        etEditItem.requestFocus();
+        etEditItem.setSelection(item.length());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSave(View v) {
+        String modifiedItem = etEditItem.getText().toString();
+        int position = getIntent().getIntExtra("position", 0);
+        Intent data = new Intent();
+        data.putExtra("modifiedItem", modifiedItem);
+        data.putExtra("position", position);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
